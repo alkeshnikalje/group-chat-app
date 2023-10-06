9d8820e3ac343af9e3b0fa663a640c188eebdff2
Alkesh Nikalje <alkeshnikalje@gmail.com>
2023-10-06 15:11:36 +0530

    styled signup component with tailwind css

--- react-chat-app/src/components/Singup.tsx
+++ react-chat-app/src/components/Singup.tsx
@@ -1,21 +1,38 @@
 export default function Signup() {
   return (
-    <form className="w-1/2 border">
-      <label>Name</label>
-      <br />
-      <input type="text" />
-      <br />
-      <label>Email</label>
-      <br />
-      <input type="email" />
-      <br />
-      <label>Phone</label>
-      <br />
-      <input type="text" />
-      <br />
-      <label>Password</label>
-      <br />
-      <input type="password" />
-    </form>
+    <div className="flex h-screen items-center justify-center">
+      <form className="w-96 rounded-lg border p-4">
+        <label className="mb-2 block text-gray-700">Name</label>
+        <input
+          type="text"
+          className="w-full rounded-lg border px-3 py-2 focus:border-blue-500 focus:outline-none"
+        />
+
+        <label className="mb-2 mt-4 block text-gray-700">Email</label>
+        <input
+          type="email"
+          className="w-full rounded-lg border px-3 py-2 focus:border-blue-500 focus:outline-none"
+        />
+
+        <label className="mb-2 mt-4 block text-gray-700">Phone</label>
+        <input
+          type="text"
+          className="w-full rounded-lg border px-3 py-2 focus:border-blue-500 focus:outline-none"
+        />
+
+        <label className="mb-2 mt-4 block text-gray-700">Password</label>
+        <input
+          type="password"
+          className="w-full rounded-lg border px-3 py-2 focus:border-blue-500 focus:outline-none"
+        />
+
+        <button
+          type="submit"
+          className="mt-6 w-full rounded-lg bg-blue-500 py-2 text-white hover:bg-blue-600 focus:outline-none"
+        >
+          Sign up
+        </button>
+      </form>
+    </div>
   );
 }
