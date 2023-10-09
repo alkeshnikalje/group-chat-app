d453cbcbf0fad4f98b25c8b95fe0fbde8ad86249
Alkesh Nikalje <alkeshnikalje@gmail.com>
2023-10-09 17:01:02 +0530

    created signin component

--- react-chat-app/src/App.tsx
+++ react-chat-app/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/Singup";
+import SignIn from "./components/Signin";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
   );
---
+++ react-chat-app/src/components/Signin.tsx
@@ -0,0 +1,26 @@
+export default function SignIn() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <form className="w-96 rounded-lg border p-4">
+        <label className="mb-2 mt-4 block text-gray-700">Email</label>
+        <input
+          type="email"
+          className="w-full rounded-lg border px-3 py-2 focus:border-blue-500 focus:outline-none"
+        />
+
+        <label className="mb-2 mt-4 block text-gray-700">Password</label>
+        <input
+          type="password"
+          className="w-full rounded-lg border px-3 py-2 focus:border-blue-500 focus:outline-none"
+        />
+
+        <button
+          type="submit"
+          className="mt-6 w-full rounded-lg bg-blue-500 py-2 text-white hover:bg-blue-600 focus:outline-none"
+        >
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
+}
